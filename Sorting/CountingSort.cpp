6989fa00ef18559fee964661135f965db4ef1ee6
Sergio Moreno <sergioalejandro.moreno04@gmail.com>
2025-03-15 23:08:29 -0600

    Refactor Counting Sort

--- Sorting/CountingSort.cpp
+++ Sorting/CountingSort.cpp
@@ -1,39 +1,52 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
 vector<int> countSort(vector<int>& inputArray)
 {
 
     int N = inputArray.size();
 
-    // Finding the maximum element of array inputArray[].
+    // Finding the maximum element of the Array
+    //*Sometimes the problem can tell you which is the Maximum element
     int M = 0;
 
     for (int i = 0; i < N; i++)
         M = max(M, inputArray[i]);
 
+
     // Initializing countArray[] with 0
+    // The range is [0,...,M+1]
     vector<int> countArray(M + 1, 0);
 
     // Mapping each element of inputArray[] as an index
     // of countArray[] array
-
     for (int i = 0; i < N; i++)
         countArray[inputArray[i]]++;
 
-    // Calculating prefix sum at every index
-    // of array countArray[]
-    for (int i = 1; i <= M; i++)
-        countArray[i] += countArray[i - 1];
-
-    // Creating outputArray[] from countArray[] array
+    // Iterating the countArray[] and inserts the index in the 
+    //outputArray[] until the countArray[] is 0
+    int idx = 0;
     vector<int> outputArray(N);
-
-    for (int i = N - 1; i >= 0; i--)
-
-    {
-        outputArray[countArray[inputArray[i]] - 1]
-            = inputArray[i];
-
-        countArray[inputArray[i]]--;
+    for(int i = 0; i < M+1; i++){
+        int count = countArray[i];
+        
+        while(count--){
+            outputArray[idx++] = i;
+        }
     }
 
     return outputArray;
+}
+int main(){
+
+    vector<int> inputArray = {0,1,3,4,2,3,4,4};
+
+    inputArray = countSort(inputArray);
+
+    for(int i : inputArray){
+        cout << i << endl;
+    }
+
+    return 0;
 }
